9eec70ece14128dd3a03ce3f85c8d16279548008
Maddy Ali <maddyoali@gmail.com>
2025-05-29 10:14:03 -0500

    Update user schema

--- src/models/User.js
+++ src/models/User.js
@@ -1,9 +1,15 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-  email: String,
-  password: String, // hashed
-  name: String,
-  isAdmin: Boolean,
-  createdAt: Date,
-});
+const UserSchema = new mongoose.Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true }, // hashed
+    name: { type: String, required: true },
+    isAdmin: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.models.User || mongoose.model("User", UserSchema);
+
+export default User;
